78ca7fe7536abd2e39503252a006e5f1be66d89a
christopherbunn <chris.bunn@alteryx.com>
2020-11-05 10:25:04 -0500

    Restructred keyboard interrupt behavior

--- evalml/automl/engines/sequential_engine.py
+++ evalml/automl/engines/sequential_engine.py
@@ -14,7 +14,7 @@ class SequentialEngine(EngineBase):
             try:
                 current_iteration = len(self.search._results['pipeline_results']) + len(evaluation_results)
                 if not self.search._check_stopping_condition(self.search._start, current_iteration):
-                    return fitted_pipelines, evaluation_results
+                    return fitted_pipelines, evaluation_results, []
 
                 if self.search.start_iteration_callback:
                     self.search.start_iteration_callback(pipeline.__class__, pipeline.parameters, self)
@@ -26,8 +26,8 @@ class SequentialEngine(EngineBase):
             except KeyboardInterrupt:
                 pipeline_batch = self.search._handle_keyboard_interrupt(pipeline_batch, pipeline)
                 if pipeline_batch == []:
-                    return fitted_pipelines, evaluation_results
-        return fitted_pipelines, evaluation_results
+                    return fitted_pipelines, evaluation_results, pipeline_batch
+        return fitted_pipelines, evaluation_results, pipeline_batch
 
     def evaluate_pipeline(self, pipeline, log_pipeline=False):
         evaluation_result = []
@@ -40,5 +40,5 @@ class SequentialEngine(EngineBase):
         except KeyboardInterrupt:
             pipeline_batch = self.search._handle_keyboard_interrupt([], pipeline)
             if pipeline_batch == []:
-                return pipeline, evaluation_result
+                return pipeline_batch, []
         return pipeline, evaluation_result
